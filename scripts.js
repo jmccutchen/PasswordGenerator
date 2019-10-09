1e2f9b5df3e459aad88dbc1d1639fe1d96f5f395
John McCutchen <purplegrits@yahoo.com>
2019-10-08 23:37:04 -0400

    a little progress on Javascript

---
+++ scripts.js
@@ -0,0 +1,40 @@
+
+function create(){
+
+    // set password length
+    var length = document.getElementById(from prompt)
+}
+
+
+// password values
+var lengthPass = prompt("How many character password do you want, 8-128");
+var special = " !\"#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+var caps = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+var lower = "abcdefghijklmnopqrstuvwxyz"
+
+var password = ""
+
+// need to set prompts to boolean values, so if True then
+
+// input validation
+if (lengthPass < 8 || lengthPass >128) {
+    alert("Choose between 8 and 128 jerky");
+} else if (lengthPass > 7 || lengthPass < 129) {
+// password creation
+    for (i = 0; i < lengthPass.length; i++){
+ //need a way to cycle through each set of values based on what a user chooses 
+    }
+}
+
+
+
+// add password to display
+document.getElementbyId("display").value = password;
+
+
+check if length is valid
+
+use .textContent to create password
+then document.body.appendChild (var)
+
+
